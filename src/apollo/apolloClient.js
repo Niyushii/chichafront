e617fa5cbbedb8a15c8a8da8aabc5f1148963c50
Niyushii <1302nicolzelaya@gmail.com>
2025-11-15 19:27:02 -0400

    inicializacion de proyecto

---
+++ src/apollo/apolloClient.js
@@ -0,0 +1,27 @@
+// src/apollo/apolloClient.js
+import { ApolloClient, InMemoryCache } from '@apollo/client/core'
+import { createUploadLink } from 'apollo-upload-client'
+import { setContext } from '@apollo/client/link/context'
+
+// token
+const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem('token')
+    return {
+        headers: {
+        ...headers,
+        Authorization: token ? `Bearer ${token}` : '',
+        },
+    }
+})
+
+// link
+const uploadLink = createUploadLink({
+    uri: 'http://127.0.0.1:8000/chichapi/',
+})
+
+const client = new ApolloClient({
+    link: authLink.concat(uploadLink),
+    cache: new InMemoryCache(),
+})
+
+export default client
